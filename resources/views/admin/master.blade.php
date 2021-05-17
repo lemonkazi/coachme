b64a2986fa6a847b1b9ddffa97fcbdec8c630c56
lemonkazi <mamun@seazoobd.com>
2021-05-17 15:45:13 +0600

    coachme added

--- resources/views/admin/master.blade.php
+++ resources/views/admin/master.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>E-Bangla</title>
+  <title>COACH ME</title>
 
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
@@ -68,14 +68,14 @@
     <!-- Sidebar -->
     <div class="sidebar">
       <!-- Sidebar user panel (optional) -->
-      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+      <!-- <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
           <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
           <a href="#" class="d-block">Abiduzzaman Abid</a>
         </div>
-      </div>
+      </div> -->
 
       <!-- SidebarSearch Form -->
       
@@ -222,7 +222,7 @@
 @yield('content')
 
 <footer class="main-footer">
-    <strong>Copyright &copy; 2021 <a href="https://adminlte.io">ABID</a>.</strong>
+    <strong>Copyright &copy; 2021.</strong>
     All rights reserved.
     <div class="float-right d-none d-sm-inline-block">
       <b>Version</b> 1.0
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>E-Bangla</title>
+    <title>COACH ME</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
