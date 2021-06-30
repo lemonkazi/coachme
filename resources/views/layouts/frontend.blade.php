42d2a1aa55f853b40684cfec3ead8f4e2fe08477
lemonkazi <mamun@seazoobd.com>
2021-06-30 15:07:16 +0600

    menu active issue solved

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -69,13 +69,13 @@
                         <a class="nav-link" href="#">About us</a>
                         </li> -->
                         <li class="nav-item">
-                            <a class="nav-link <?php if ($controller == 'publiccontroller' && $action == 'coach_list') echo 'active' ?>" href="{{ url('/coach/list') }}">Coaches</a>
+                            <a class="nav-link <?php if ($controller == 'publiccontoller' && $action == 'coach_list') echo 'active' ?>" href="{{ url('/coach/list') }}">Coaches</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link <?php if ($controller == 'publiccontroller' && $action == 'camp_list') echo 'active' ?>" href="{{ url('/camp/list') }}">Camps</a>
+                            <a class="nav-link <?php if ($controller == 'publiccontoller' && $action == 'camp_list') echo 'active' ?>" href="{{ url('/camp/list') }}">Camps</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link <?php if ($controller == 'publiccontroller' && $action == 'program_list') echo 'active' ?>" href="{{ url('/program/list') }}">Programs</a>
+                            <a class="nav-link <?php if ($controller == 'publiccontoller' && $action == 'program_list') echo 'active' ?>" href="{{ url('/program/list') }}">Programs</a>
                         </li>
                         <li>
                             @if (Route::has('logout'))
