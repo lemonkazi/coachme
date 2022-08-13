3a6c650853ced6f69c5db2f550990c80d4dd8512
lemonkazi <lemonpstu09@gmail.com>
2022-08-13 15:22:33 +0600

    Feat(server-file): :pencil: certificate name and website added in migration

---
+++ database/migrations/2022_08_13_091452_change_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ChangeUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('certificate_name', 50)->nullable()->after('certificate_id');
+            $table->string('website', 100)->nullable()->after('phone_number');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('certificate_name');
+            $table->dropColumn('website');
+        });
+    }
+}
